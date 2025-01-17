00ea4326557f57eff30b17cc7dce9e121430e735
Aaron hebert <aaron.hebert@ted.com>
2025-01-17 12:34:25 -0500

    set telescope/fd search params

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,13 @@ return	{
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
+		require('telescope').setup({
+			pickers = {
+				find_files = {
+    			find_command = { "fd", "--hidden", "--glob", "" },
+  			},
+			}
+		})
     vim.keymap.set('n', '<leader>p', builtin.find_files, {})
     vim.keymap.set('n', '<leader>s', function()
 	    builtin.grep_string({ search = vim.fn.input("Grep > ") });
