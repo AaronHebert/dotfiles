3205c61acaa4ef9905040c51639edd1d80654374
Aaron hebert <aaron.hebert@ted.com>
2025-01-24 16:29:37 -0500

    new setup.sh file in progress...

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+### symlinks
+ls -s .fdignore ~/.fdignore
+ln -s .gemrc ~/.gemrc
+ln -s .hushlogin ~/.hushlogin
+ln -s .irbrc ~/.irbrc
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .zshrc ~/.zshrc.base
