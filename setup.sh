c7dbc2d7f3403948e510cc5742c332b39e654379
Aaron hebert <aaron.hebert@ted.com>
2025-07-31 09:52:51 -0400

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 
+#### Install Brew
++/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
 #### Brew packages
 brew bundle
 
@@ -12,12 +16,12 @@ echo "source $PWD/.zshrc.base" > ~/.zshrc
 
 #### Symlinks
 ln -sf .fdignore ~/.fdignore
-ln -sf .gemrc ~/.gemrc
-ln -sf .gitconfig ~/.gitconfig
-ln -sf .hushlogin ~/.hushlogin
-ln -sf .irbrc ~/.irbrc
-ln -sf .tmux.conf ~/.tmux.conf
+ln -sf $PWD/.gemrc ~/.gemrc
+ln -sf $PWD/.hushlogin ~/.hushlogin
+ln -sf $PWD/.irbrc ~/.irbrc
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
 
+ln -sf $PWD/config/nvim ~/.config/nvim
 
 #### Home setup
 mkdir -p ~/.secrets/rails_credentials
@@ -29,5 +33,5 @@ mkdir ~/.vimwiki
 #curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 
 
-#### tpm (tmux plugin manager)
+#### tpm (tmux plugin manager) - in tmux, prefix + I to install
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
