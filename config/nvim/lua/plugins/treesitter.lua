39f1568826094f19bd8e159c0c8b7165d4d0af8b
Aaron Hebert <aaronhebert@me.com>
2026-05-29 15:54:17 -0400

    Thanks for breaking things, nvim-treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    config = function () 
-      local config = require("nvim-treesitter.configs")
-
-      config.setup({
-        ensure_installed = { "ruby", "lua", "vim", "vimdoc", "javascript", "html" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-        })
-   	end 
-  }
+	{
+		"romus204/tree-sitter-manager.nvim",
+		dependencies = {}, -- tree-sitter CLI must be installed system-wide
+		config = function()
+			require("tree-sitter-manager").setup({
+				-- Default Options
+				-- ensure_installed = {}, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
+				-- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+				-- auto_install = false, -- if enabled, install missing parsers when editing a new file
+				-- highlight = true, -- treesitter highlighting is enabled by default
+				-- languages = {}, -- override or add new parser sources
+			})
+		end
+	}
 }
